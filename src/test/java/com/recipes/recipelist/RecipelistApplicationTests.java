491caccfb2005d44995c67a81c1a2ef175282e5c
chris <104324755+cjurgens17@users.noreply.github.com>
2023-01-12 13:32:06 -0500

    pusing initial project up

---
+++ src/test/java/com/recipes/recipelist/RecipelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipes.recipelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
